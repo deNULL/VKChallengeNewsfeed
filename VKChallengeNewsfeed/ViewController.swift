2e08ae7d588bf13c82ebe65d8983a02a30e5058e
Denis Olshin <me@denull.ru>
2018-11-09 00:59:37 +0300

    Initial Commit

---
+++ VKChallengeNewsfeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKChallengeNewsfeed
+//
+//  Created by Denis Olshin on 09/11/2018.
+//  Copyright © 2018 Denis Olshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
